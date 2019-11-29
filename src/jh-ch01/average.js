3951c6986df9c167a3b1929db7189bcffb263e76
dev_pjh-at-670977422354 <jjjhhhvvv@gmail.com>
2019-11-29 14:15:53 +0900

    챕터-2 실습 완료

--- src/jh-ch01/average.js
+++ src/jh-ch01/average.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 
 //  학생들의 평균 점수를 구하여라
 const input = [80, 90, 100];
@@ -21,4 +22,56 @@ const size = arr => arr.length;
 const device = (a, b) => a / b;
 const average = arr => device(total(arr), size(arr));
 console.log('함수형 프로그래밍 평균점수:', average(input));
+
+
+// 문제2. 복수 과목을 수강한 학생의 평균 구하기
+const students = [
+  {grade: 100, enrolled: 2},
+  {grade: 80, enrolled: 2},
+  {grade: 80, enrolled: 1},
+];
+
+// 2-1 명령형 관점에서 풀기
+let totalGrade = 0, totalEnrolled = 0;
+for (let i = 0; i < students.length; i++) {
+  const student = students[i];
+  if (student) {
+    if (student.enrolled > 1) {
+      totalGrade += student.grade;
+      totalEnrolled++;
+    }
+  }
+}
+console.log('명령형 관점에서 풀기 average2: ', totalGrade / totalEnrolled);
+
+// 2-2  함수형 프로그래밍 관점
+// _.chain(students)
+//   .filter(student => student.enrolled > 1)
+//   .pluck('grade')
+//   .average()
+//   .value();
+// => 체이닝을 통해 느슨한 평가를 한다.
+
+
+
+function Person(firstname, lastname, age) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.age = age;
+}
+
+const man1 = new Person('james', 'boby', 20);
+
+// 객체의 값을 조작하지 않고 값만 리턴한다는 컨셉을 보여줌
+const fullname = person => [person.firstname, person.lastname].join(' ');
+console.log(fullname(man1), man1); // man1의 프로퍼티를 조작하지 않음.
+// 배열에 담아서 조인하여 리턴한다.?  객체의 값들을 복사해서 원본 데이터를 조작하지 않으려는 의지로 해석됨.
+
+
+
+
+
+
+
+
 
